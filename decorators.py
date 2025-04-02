c816457b92f8ecb94ff9513d1630689de11d8133
qwerty1121gif <Vladimirstalin@tutamail.com>
2025-04-02 21:13:34 +0300

    kee

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+# decorators.py
 import os
 import pandas as pd
 from datetime import datetime
@@ -5,18 +6,15 @@ import uuid
 from functools import wraps
 
 def logged(func):
-
     @wraps(func)
     def wrapper(*args, **kwargs):
-        # Сбор данных
         log_id = str(uuid.uuid4())
         username = os.getlogin()
-        function_name = func.name
+        function_name = func.__name__
         current_time = datetime.now()
         date_str = current_time.strftime("%d.%m.%Y")
         time_str = current_time.strftime("%H:%M:%S")
 
-        # Создание DataFrame для новой записи
         new_entry = pd.DataFrame({
             "id": [log_id],
             "pc username": [username],
@@ -25,17 +23,12 @@ def logged(func):
             "Time": [time_str]
         })
 
-        # Запись в CSV
         try:
-            # Если файл существует, читаем его и добавляем новую запись
             existing_log = pd.read_csv("logs.csv")
             updated_log = pd.concat([existing_log, new_entry], ignore_index=True)
         except FileNotFoundError:
-            # Если файла нет, создаем новый
             updated_log = new_entry
 
         updated_log.to_csv("logs.csv", index=False)
-
-        # Вызов исходной функции
         return func(*args, **kwargs)
     return wrapper
